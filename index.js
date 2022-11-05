87bac5f2b2c373930d1dc4711a6f2eb14f9e1a00
William Davidson <wj.davidson@outlook.com>
2022-11-05 21:01:12 +0100

    updating nav bar and cursour

---
+++ index.js
@@ -0,0 +1,49 @@
+const profileBtn = document.querySelector(".profileImg");
+const box = document.querySelectorAll(".navli");
+
+
+profileBtn.addEventListener("click", () => {
+    profileBtn.classList.toggle("glowicon");
+    box.forEach((e) => {
+        e.classList.toggle("navtoggle")
+    })
+})
+
+
+const trailer = document.getElementById("trailer");
+
+const animateTrailer = (e, interacting) => {
+  const x = e.clientX - trailer.offsetWidth / 2,
+        y = e.clientY - trailer.offsetHeight / 2;
+  
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px) scale(${interacting ? 8 : 1})`
+  }
+  
+  trailer.animate(keyframes, { 
+    duration: 0, 
+    fill: "forwards" 
+  });
+}
+
+const getTrailerClass = type => {
+  switch(type) {
+    case "video":
+      return "fa-solid fa-play";
+    default:
+      return "fa-solid fa-arrow-up-right"; 
+  }
+}
+
+window.onmousemove = e => {
+  const interactable = e.target.closest(".interactable"),
+        interacting = interactable !== null;
+    
+  animateTrailer(e, interacting);
+  
+  trailer.dataset.type = interacting ? interactable.dataset.type : "";
+  
+  if(interacting) {
+    icon.className = getTrailerClass(interactable.dataset.type);
+  }
+}
