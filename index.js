158010be915dadac2c427da4d2e6f670844f1263
William Davidson <wj.davidson@outlook.com>
2022-11-06 01:46:46 +0100

    updating files

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const profileBtn = document.querySelector(".profileImg");
 const box = document.querySelectorAll(".navli");
 
-
 profileBtn.addEventListener("click", () => {
     profileBtn.classList.toggle("glowicon");
     box.forEach((e) => {
@@ -9,41 +8,11 @@ profileBtn.addEventListener("click", () => {
     })
 })
 
-
-const trailer = document.getElementById("trailer");
-
-const animateTrailer = (e, interacting) => {
-  const x = e.clientX - trailer.offsetWidth / 2,
-        y = e.clientY - trailer.offsetHeight / 2;
-  
-  const keyframes = {
-    transform: `translate(${x}px, ${y}px) scale(${interacting ? 8 : 1})`
-  }
-  
-  trailer.animate(keyframes, { 
-    duration: 0, 
-    fill: "forwards" 
-  });
-}
-
-const getTrailerClass = type => {
-  switch(type) {
-    case "video":
-      return "fa-solid fa-play";
-    default:
-      return "fa-solid fa-arrow-up-right"; 
-  }
-}
-
-window.onmousemove = e => {
-  const interactable = e.target.closest(".interactable"),
-        interacting = interactable !== null;
-    
-  animateTrailer(e, interacting);
-  
-  trailer.dataset.type = interacting ? interactable.dataset.type : "";
-  
-  if(interacting) {
-    icon.className = getTrailerClass(interactable.dataset.type);
-  }
-}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
